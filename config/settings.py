381625593321ffde90f636e37e184223b892032e
kayubin <kayubin@gmail.com>
2026-08-30 01:21:28 +0900

    feat: secret key env파일로 옮김

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)q!n@=3u@&-tyw6m@2s%0fvg5liq9j9z-fh@wnabde+$t)gg^!'
+SECRET_KEY = decouple("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
