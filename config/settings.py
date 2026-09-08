5504c71249c1212fb3a83d0620c6519200acec81
kayubin <kayubin@gmail.com>
2026-09-08 21:35:14 +0900

    docs: cors 연결 수정

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,15 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "tourcontest-backend.onrender.com",
+    "tourcontest-frontend.onrender.com",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://tourcontest-frontend.onrender.com",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -71,6 +78,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',   
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
